77f971aa091c206ec62590c19d6735d391498798
James Hosken <jameswfhosken@gmail.com>
2018-11-21 16:24:09 -0600

    Add blender threading test (Works!)

---
+++ Scratch/blenderThreadingTest.py
@@ -0,0 +1,17 @@
+import serial
+import time
+import threading
+
+ser = serial.Serial("COM4", 9600)
+
+
+def readSerialData():
+	while True:
+		print(ser.readline())
+
+def doOtherStuff():
+	print("Hello! I am other stuff")
+
+threading.Thread(target=readSerialData).start()
+
+doOtherStuff()
